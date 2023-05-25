b0869592de2aab9d403881560b87017abf44dc06
Big Mac 7 <bigmac7@bigs-imac.home>
2023-05-25 14:37:29 -0700

    add useEffect to filter countries

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,47 @@ const Home = () => {
   const [selectedRegion, setSelectedRegion] = useState("All");
   const [searchInput, setSearchInput] = useState("");
 
-  return <div>Home</div>;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("/data.json");
+        const data = await res.json();
+        setCountries(data);
+        setFilteredCountries(data);
+      } catch (err) {
+        console.log("Error fetching country data: error");
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    const filtered = countries.filter((country) => {
+      const searchTermMatch = country.name
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase());
+
+      const regionMatch =
+        selectedRegion === "All" || country.region === selectedRegion;
+
+      return searchTermMatch & regionMatch;
+    });
+    setFilteredCountries(filtered);
+  }, [searchTerm, selectedRegion, countries]);
+
+  return (
+    <>
+      <input type="text" placeholder="Search for a country..." />
+
+      <select>
+        <option value="All">Filter by Region</option>
+        <option value="Africa">Africa</option>
+        <option value="Americas">Americas</option>
+        <option value="Asia">Asia</option>
+        <option value="Europe">Europe</option>
+        <option value="Oceania">Oceania</option>
+      </select>
+    </>
+  );
 };
 
 export default Home;
