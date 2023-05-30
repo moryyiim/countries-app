abc42eb9a913a6ac953fe498348e1723a8ed9145
Big Mac 7 <bigmac7@bigs-imac.home>
2023-05-30 09:56:58 -0700

    make 4 column grid layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -66,13 +66,17 @@ const Home = () => {
   return (
     <>
       <div class="search-filter">
-        <input
-          value={searchInput}
-          onChange={handleChange}
-          onKeyDown={handleKeyDown}
-          type="text"
-          placeholder="Search for a country..."
-        />
+        <div class="search-box">
+          <input
+            class="search-input"
+            value={searchInput}
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+            type="text"
+            placeholder="Search for a country..."
+          />
+          <i class="fa fa-search"></i>
+        </div>
 
         <select value={selectedRegion} onChange={handleRegionChange}>
           <option value="All">Filter by Region</option>
@@ -84,26 +88,32 @@ const Home = () => {
         </select>
       </div>
 
-      <ul className="countries-list">
-        {filteredCountries.map((country) => (
-          <li key={country.alpha3Code}>
-            <img src={country.flag} alt={country.name} />
-            <div className="country-details">
-              <h2>{country.name}</h2>
-              <p>
-                <strong>Population:</strong>
-                {country.population}
-              </p>
-              <p>
-                <strong>Region:</strong> {country.region}
-              </p>
-              <p>
-                <strong>Capital:</strong> {country.capital}
-              </p>
-            </div>
-          </li>
-        ))}
-      </ul>
+      <div className="countries-container">
+        <ul className="countries-list">
+          {filteredCountries.map((country) => (
+            <li key={country.alpha3Code}>
+              <img
+                class="countries-img"
+                src={country.flag}
+                alt={country.name}
+              />
+              <div className="country-details">
+                <h2>{country.name}</h2>
+                <p>
+                  <strong>Population:</strong>
+                  {country.population}
+                </p>
+                <p>
+                  <strong>Region:</strong> {country.region}
+                </p>
+                <p>
+                  <strong>Capital:</strong> {country.capital}
+                </p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
